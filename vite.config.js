4753dc6a27ccef7d08fe1ee925ef0b62e8447a78
bertyn99 <bboulikou@gmail.com>
2023-01-05 16:27:51 +0100

    ajout proxy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,11 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api': 'http://localhost:3000',
+    }
+  },
   plugins: [
     react(),
     VitePWA({
