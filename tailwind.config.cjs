e1da3bdf0df54060a9c6a1d7202f2266ada37787
bertyn99 <bboulikou@gmail.com>
2023-01-07 03:04:55 +0100

    add color tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#d5d6e0",
+          200: "#aaadc0",
+          300: "#8084a1",
+          400: "#555b81",
+          500: "#2b3262",
+          600: "#22284e",
+          700: "#1a1e3b",
+          800: "#111427",
+          900: "#090a14",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
