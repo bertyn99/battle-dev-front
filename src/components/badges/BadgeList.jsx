852c6009f4d151c6ebc3989f41582b92794138d3
bertyn99 <bboulikou@gmail.com>
2023-01-10 00:29:05 +0100

    fix no badges

--- src/components/badges/BadgeList.jsx
+++ src/components/badges/BadgeList.jsx
@@ -5,12 +5,12 @@ import { SlBadge } from "react-icons/sl";
 const list = ["a", "b", "c", "c", "d", "d", "e", "s", "s"];
 function BadgeList({ badges }) {
   return (
-    <div className="mt-2 ">
+    <div className="mt-3 ">
       <h2 className="text-center font-medium mb-6">Badges</h2>
       {badges?.length > 0 ? (
         <List list={badges} renderItem={(data) => <Badge data={data} />}></List>
       ) : (
-        <p className="h-20 w-full  bg-gray-900/50 inline-flex justify-center items-center gap-2 p-2 font-medium text-lg">
+        <p className="h-36 w-full  bg-gray-900/50 inline-flex justify-center items-center gap-2 p-2 font-medium text-lg">
           <SlBadge className="h-6 w-6" /> No Badges Earned
         </p>
       )}
