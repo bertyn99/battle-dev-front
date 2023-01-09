463bf6ebbae9a50c3aa3c0f3f445f5b00b25b55d
bertyn99 <bboulikou@gmail.com>
2023-01-10 00:30:08 +0100

    header ranking board

--- src/page/board.jsx
+++ src/page/board.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { BsTrophyFill } from "react-icons/bs";
 function Board() {
-  return <div>Bard</div>;
+  return (
+    <div className="h-full">
+      <div className="flex flex-col justify-center items-center">
+        <BsTrophyFill className="h-36 w-36 text-indigo-500" />
+        <h2>Ranking Board</h2>
+      </div>
+
+      <div></div>
+    </div>
+  );
 }
 
 export default Board;
