634eeda4f4bfbe4a1bb41c0303b66ae306fde01e
bertyn99 <bboulikou@gmail.com>
2023-01-12 23:38:48 +0100

    form for creat a quizz battle

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import BaseButton from "../components/common/BaseButton";
 import Selector from "../components/common/Selector";
 import { useLoaderData } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setQuizz } from "../store/slice/quizzSlice";
 function Home() {
   const category = useLoaderData();
+  const navigate = useNavigate();
+  const [selectedCat, setCategory] = useState(null);
+  const [mode, setMode] = useState("");
+  const dispatch = useDispatch();
+  const isActive = (text) => mode == text;
 
+  const play = () => {
+    dispatch(setQuizz({ mode, category: selectedCat }));
+    navigate("/quizz");
+  };
   return (
     <div className=" max-w-2xl mx-auto px-4 flex flex-col gap-6 ">
       <div>
@@ -17,19 +29,35 @@ function Home() {
           Select a Category
         </h2>
 
-        <Selector list={category} />
+        <Selector
+          list={category}
+          selected={selectedCat}
+          setSelected={setCategory}
+        />
       </div>
       <div className="my-4">
         <h2 className="font-semibold text-xl text-center mb-2">
           Select a mode
         </h2>
         <div className="flex gap-2">
-          <BaseButton variant="secondary">Solo</BaseButton>
-          <BaseButton variant="secondary">Multi</BaseButton>
+          <BaseButton
+            variant={isActive("Solo") ? "secondary-active" : "secondary"}
+            onClick={() => setMode("Solo")}
+          >
+            Solo
+          </BaseButton>
+          <BaseButton
+            variant={isActive("Multi") ? "secondary-active" : "secondary"}
+            onClick={() => setMode("Multi")}
+          >
+            Multi
+          </BaseButton>
         </div>
       </div>
 
-      <BaseButton>Start a new Battle</BaseButton>
+      <BaseButton disabled={selectedCat == null || mode == ""} onClick={play}>
+        Start a new Battle
+      </BaseButton>
     </div>
   );
 }
