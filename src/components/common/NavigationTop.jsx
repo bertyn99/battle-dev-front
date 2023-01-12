ba5fc6abee491f833586c6c0622b7379f646981f
bertyn99 <bboulikou@gmail.com>
2023-01-12 12:26:19 +0100

    add go back button

--- src/components/common/NavigationTop.jsx
+++ src/components/common/NavigationTop.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { BsChevronCompactLeft, BsThreeDotsVertical } from "react-icons/bs";
 function NavigationTop() {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
-    <nav className='h-9 bg-white/5 flex'>Navigation</nav>
-  )
+    <nav className="h-9 bg-white/0 flex justify-between items-center px-4 mt-1">
+      <BsChevronCompactLeft onClick={goBack} />
+      <BsThreeDotsVertical />
+    </nav>
+  );
 }
 
-export default NavigationTop
+export default NavigationTop;
