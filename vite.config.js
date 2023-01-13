ca72915914dd1efd8eefd7e32ef05b6fec907f39
bertyn99 <bboulikou@gmail.com>
2023-01-13 01:40:37 +0100

    add env url

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
-    },
-  },
-  plugins: [
-    react(),
-    VitePWA({
-      devOptions: {
-        enabled: true,
-        /* other options */
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    server: {
+      proxy: {
+        "/api": env.VITE_API_BASE_URL,
       },
-    }),
-  ],
+    },
+    plugins: [
+      react(),
+      VitePWA({
+        devOptions: {
+          enabled: true,
+          /* other options */
+        },
+      }),
+    ],
+  };
 });
