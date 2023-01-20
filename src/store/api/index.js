3f06f111f1afebec3e6ce535b64fad4ae8da2fde
Bertyn Boulikou <bboulikou@gmail.com>
2023-01-20 10:09:50 +0100

    fix temporaire

--- src/store/api/index.js
+++ src/store/api/index.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "../slice/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "/api",
+  baseUrl: "https://back-battle-dev.onrender.com/api",
   /*    mode: 'cors',
     headers: {
       'Access-Control-Allow-Origin':'*'
